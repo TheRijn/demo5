8597ce7293f4cc29200b74c9f7dd47b84ecd3124
Marijn Doeve <marijn@doeve.me>
2021-03-12 11:50:52 +0100

    models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.ManyToManyField("Author", related_name="books")
+    isbn = models.CharField(max_length=13)
+    year = models.SmallIntegerField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    birthday = models.DateField()
+
+
+class Review(models.Model):
+    score = models.PositiveIntegerField()
+    review_text = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='reviews')
+
+# class book_author(models.Model):
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # book = models.ForeignKey(Book, on_delete=models.CASCADE)
